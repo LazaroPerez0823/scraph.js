45c73a0fcf13abd04bda3895d2b45b81ff35a7fd
Lazaro <Lazaro.Perez0823@gmail.com>
2019-10-22 16:38:31 -0400

    Trivia - still need a change

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -4,48 +4,81 @@ window.onload = function () {
   //declaring variables
   var correctN = 0;
   var incorrectN = 0;
-  var unanswered;
-  var timeRemaining = 5;
+  var unansweredN = 0;
+  var timeRemaining = 30;
   var intervalId;
+  var timerRunning = false;
   $("#startGame").on("click", start);
-  $("#doneGame").on("click", stop);
+
 
   // 
 
   function start() {
+    if (!timerRunning) {
+      // clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+      timerRunning = true;
+      $(".startButtonArea").hide();
+      $("#mainArea").show();
 
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-    $(".startButtonArea").hide();
-    $("#mainArea").show();
-
+    }
 
     function decrement() {
       $("#timer").html(timeRemaining);
       timeRemaining--;
 
-      if (timeRemaining === 0) {
+      if (timeRemaining < 0) {
+
+
 
         stop();
       }
     }
 
+    $("#doneGame").on("click", stop);
 
     function stop() {
-
+      clearInterval(intervalId);
+      timerRunning = false;
       var i;
-      var answers; 
-        for (i = 1; i < 5; i++) {
+      var answers;
+      var wrong;
+
+
+      for (i = 1; i < 6; i++) {
         var answers = ("#correct" + [i])
+        var wrong = (".incorrect" + [i])
+
         if ($(answers).prop('checked')) {
           correctN++
-        } else {
-          incorrectN++
         }
-  
+
+        else if ($(wrong).prop('checked')) {
+          incorrectN++
+        } else {
+          unansweredN++;
+        }
+
       }
 
-      displayScores();
+      // if ($('input[name="question1"]:checked').length < 1) {
+      //     unansweredN++; 
+      //   }
+      //   if ($('input[name="question2"]:checked').length < 1) {
+      //     unansweredN++; 
+      //   }
+      //   if ($('input[name="question3"]:checked').length < 1) {
+      //     unansweredN++; 
+      //   }
+      //   if ($('input[name="question4"]:checked').length < 1) {
+      //     unansweredN++; 
+      //   }
+
+      $("#correctAnswersText").append(correctN);
+      $("#incorrectAnswersText").append(incorrectN);
+      $("#unansweredText").append(unansweredN);
+      $("#gameQuestions").hide();
+      $("#gameResults").show();
 
 
     }
@@ -53,9 +86,15 @@ window.onload = function () {
 
   }
 
-  function displayScores() {
-    console.log(correctN);
-    console.log(incorrectN);
+  $("#playAgain").on("click", playAgain);
 
+  function playAgain() {
+
+    location.reload();
   }
+
+
+
+
+
 }
